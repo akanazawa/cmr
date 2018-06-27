035567069fb3ee1ec6d7297423e2273f369b82fc
Shubham Tulsiani <shubhtuls@gmail.com>
2018-06-26 20:57:26 -0700

    external deps

---
+++ external/install_external.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/shubhtuls/PerceptualSimilarity
+
+git clone https://github.com/hiroharu-kato/neural_renderer --branch v1.1.0
+cd neural_renderer
+python setup.py install
